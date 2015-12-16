af2d7269249c5b386763601f63d0db54ea87908c
Rafael Slobodian <rslobodian@pivotal.io>
2015-12-16 17:25:33 -0500

    RS - Added methods to retrieve a mysql instance from CF.

---
+++ db/datasource.go
@@ -0,0 +1,49 @@
+package db
+import (
+	"database/sql"
+	"github.com/cloudfoundry-community/go-cfenv"
+	"fmt"
+	"github.com/xtreme-rafael/safenotes-api/utils"
+	_ "github.com/go-sql-driver/mysql"
+	"errors"
+)
+
+const (
+	PackageName = "DB"
+	mysqlDataSourceStringFormat = "%s:%s@tcp(%s:%s)/%s?parseTime=true"
+)
+
+func GetCloudFoundryDatabase() (*sql.DB, error) {
+	dataSourceName := getCloudFoundryDataSource();
+	if dataSourceName == "" {
+		err := errors.New("Unable to retrieve database credentials from CF")
+		utils.Log(PackageName, err.Error())
+		return nil, err
+	}
+
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		err = errors.New("Unable to open connection with DB - " + err.Error())
+		utils.Log(PackageName, err.Error())
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func getCloudFoundryDataSource() string {
+	env, _ := cfenv.Current()
+	if env != nil {
+		mysqlService, _ := env.Services.WithTag("mysql")
+		credentials := mysqlService[0].Credentials
+		dataSourceName := fmt.Sprintf(mysqlDataSourceStringFormat,
+			credentials["username"],
+			credentials["password"],
+			credentials["hostname"],
+			credentials["port"],
+			credentials["name"])
+		return dataSourceName
+	}
+
+	return ""
+}
