b6fc238c4eec62f8c67bbb69acbb42824f912c2f
Rafael Slobodian <rslobodian@pivotal.io>
2015-12-16 17:25:07 -0500

    RS - Added logging utility.

---
+++ utils/log.go
@@ -0,0 +1,11 @@
+package utils
+import (
+	"fmt"
+	"log"
+)
+
+func Log(packageName string, stringFormat string, stringArgs...interface{}) {
+	message := fmt.Sprintf(stringFormat, stringArgs...)
+	message = fmt.Sprintf("[%s] %s", packageName, message)
+	log.Println(message)
+}
